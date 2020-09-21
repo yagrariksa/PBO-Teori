52618005d26dc2df4735a93211a9b39a7b21fbaf
yagrariksa <luckyrama0@gmail.com>
2020-09-21 16:11:45 +0700

    mau kumpulin tugas1 via email

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 
-print('hello world')
+class Luas:
+	def persegi(x):
+		luas = x**2
+		print("luas persegi : ",luas)
+		return None
+	def lingkaran(x):
+		pi = 22/7
+		kuadrat = x**2
+		luas = pi*kuadrat
+		print("luas lingkaran : ",luas)
+
+Luas.persegi(4)
+Luas.lingkaran(7)
